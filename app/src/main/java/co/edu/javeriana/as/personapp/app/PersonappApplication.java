382e1a4c848100bdf45a6bca4f413b17431bd419
Andres Sanchez-Martin <andres.karoll@gmail.com>
2022-05-06 21:26:45 -0500

    init

---
+++ app/src/main/java/co/edu/javeriana/as/personapp/app/PersonappApplication.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.as.personapp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonappApplication.class, args);
+	}
+
+}
