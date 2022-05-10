f14743ad89adcb2b268632c12ef64484bb544bfc
2qualcuno7 <suarezmjuan@gmail.com>
2022-05-10 08:13:10 -0500

    Gettes and setters Estudio

--- core/src/main/java/co/edu/javeriana/as/personapp/core/domain/Estudio.java
+++ core/src/main/java/co/edu/javeriana/as/personapp/core/domain/Estudio.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Estudio {
+    //Attributes
     private Integer id;
     private Integer ccPer;
     private Date fecha;
     private String univer;
 
+    //Constructors
     public Estudio() {
         super();
         this.id = 0;
@@ -24,4 +26,39 @@ public class Estudio {
         this.fecha = fecha;
         this.univer = univer;
     }
+
+    //Getters and Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCcPer() {
+        return ccPer;
+    }
+
+    public void setCcPer(Integer ccPer) {
+        this.ccPer = ccPer;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getUniver() {
+        return univer;
+    }
+
+    public void setUniver(String univer) {
+        this.univer = univer;
+    }
+
+
 }
