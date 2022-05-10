acf8a500db66eccb080fdb15454f889767d1e662
2qualcuno7 <suarezmjuan@gmail.com>
2022-05-10 08:10:48 -0500

    Attributes and constructors Estudio

--- core/src/main/java/co/edu/javeriana/as/personapp/core/domain/Estudio.java
+++ core/src/main/java/co/edu/javeriana/as/personapp/core/domain/Estudio.java
@@ -1,5 +1,27 @@
 package co.edu.javeriana.as.personapp.core.domain;
 
-public class Estudio {
+import java.util.ArrayList;
+import java.util.Date;
 
+public class Estudio {
+    private Integer id;
+    private Integer ccPer;
+    private Date fecha;
+    private String univer;
+
+    public Estudio() {
+        super();
+        this.id = 0;
+        this.ccPer = 0;
+        this.fecha = new Date();
+        this.univer = "";
+    }
+
+    public Estudio(Integer id, Integer ccPer, Date fecha, String univer) {
+        super();
+        this.id = id;
+        this.ccPer = ccPer;
+        this.fecha = fecha;
+        this.univer = univer;
+    }
 }
