95ce9848f5b645ab2dd1db7c055c04311f5e38fe
2qualcuno7 <suarezmjuan@gmail.com>
2022-05-10 08:30:17 -0500

    Constructors Profesion

--- core/src/main/java/co/edu/javeriana/as/personapp/core/domain/Profesion.java
+++ core/src/main/java/co/edu/javeriana/as/personapp/core/domain/Profesion.java
@@ -1,5 +1,6 @@
 package co.edu.javeriana.as.personapp.core.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Profesion {
@@ -8,4 +9,27 @@ public class Profesion {
     private String nom;
     private String des;
     private List<Telefono> telefonos;
+
+    //Constructors
+
+    public Profesion() {
+        this.id = 0;
+        this.nom = "";
+        this.des = "";
+        this.telefonos = new ArrayList<Telefono>();
+    }
+
+    public Profesion(Integer id, String nom, String des) {
+        this.id = id;
+        this.nom = nom;
+        this.des = des;
+        this.telefonos = new ArrayList<Telefono>();
+    }
+
+    public Profesion(Integer id, String nom, String des, List<Telefono> telefonos) {
+        this.id = id;
+        this.nom = nom;
+        this.des = des;
+        this.telefonos = telefonos;
+    }
 }
