8b792872bd78179c38e4fd686daf10262ddeea86
2qualcuno7 <suarezmjuan@gmail.com>
2022-05-11 06:26:30 -0500

    Profesion use case

--- core/src/main/java/co/edu/javeriana/as/personapp/core/usecase/ProfesionUseCase.java
+++ core/src/main/java/co/edu/javeriana/as/personapp/core/usecase/ProfesionUseCase.java
@@ -1,5 +1,21 @@
 package co.edu.javeriana.as.personapp.core.usecase;
 
-public interface ProfesionUseCase {
+import co.edu.javeriana.as.personapp.core.domain.Estudio;
+import co.edu.javeriana.as.personapp.core.domain.Persona;
+import co.edu.javeriana.as.personapp.core.domain.Profesion;
 
+import java.util.List;
+
+public interface ProfesionUseCase {
+    public Profesion buscarPorId(Integer id);
+
+    public List<Profesion> buscarTodo();
+
+    public Integer contar();
+
+    public Profesion crear(Profesion profesion);
+
+    public Profesion editar(Integer id, Profesion profesion);
+
+    public Boolean eliminar(Integer is);
 }
