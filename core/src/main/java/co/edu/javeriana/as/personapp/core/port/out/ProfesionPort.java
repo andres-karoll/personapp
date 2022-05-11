648af28c18a7ac3911d58dd8cd6376761fc2622b
2qualcuno7 <suarezmjuan@gmail.com>
2022-05-11 06:44:29 -0500

    Profesion ports

--- core/src/main/java/co/edu/javeriana/as/personapp/core/port/out/ProfesionPort.java
+++ core/src/main/java/co/edu/javeriana/as/personapp/core/port/out/ProfesionPort.java
@@ -1,4 +1,18 @@
 package co.edu.javeriana.as.personapp.core.port.out;
 
+import co.edu.javeriana.as.personapp.core.domain.Persona;
+import co.edu.javeriana.as.personapp.core.domain.Profesion;
+
+import java.util.List;
+
 public interface ProfesionPort {
+    public Profesion save(Profesion profesion);
+
+    public Boolean delete(Integer id);
+
+    public List<Profesion> findAll();
+
+    public Profesion findByCC(Integer id);
+
+    public Integer count();
 }
---
+++ core/src/main/java/co/edu/javeriana/as/personapp/core/port/out/mysql/ProfesionMySQLPort.java
@@ -0,0 +1,6 @@
+package co.edu.javeriana.as.personapp.core.port.out.mysql;
+
+import co.edu.javeriana.as.personapp.core.port.out.ProfesionPort;
+
+public interface ProfesionMySQLPort extends ProfesionPort {
+}
