e9b2ece0de5f89b9649ab4bce1793d2e43f294f4
ValentinaRozo <valerozobernal@gmail.com>
2022-05-12 17:30:08 -0500

    Linea

--- rest/src/main/java/co/edu/javeriana/as/personapp/rest/MainRest.java
+++ rest/src/main/java/co/edu/javeriana/as/personapp/rest/MainRest.java
@@ -3,12 +3,15 @@ package co.edu.javeriana.as.personapp.rest;
 import co.edu.javeriana.as.personapp.core.domain.Genero;
 import co.edu.javeriana.as.personapp.core.domain.Persona;
 import co.edu.javeriana.as.personapp.rest.client.PersonaClient;
+import co.edu.javeriana.as.personapp.rest.mapper.PersonaMapper;
 import org.springframework.boot.SpringApplication;
 
 public class MainRest {
     public static void main(String[] args) {
         PersonaClient personaclient = new PersonaClient();
+        PersonaMapper personaMapper = new PersonaMapper();
         Persona persona = new Persona(123,"nombre", "apellido", Genero.valueOf("FEMENINO"), 3);
+        personaclient.create(personaMapper.dePersonaAJSONObject(persona));
         //personaclient.create(persona);
     }
 
