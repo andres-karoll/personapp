df0c43deef4b5014a28cb7ab5ac10b59885d10c3
2qualcuno7 <suarezmjuan@gmail.com>
2022-05-12 08:25:51 -0500

    Persona service

--- core/src/main/java/co/edu/javeriana/as/personapp/core/service/PersonaService.java
+++ core/src/main/java/co/edu/javeriana/as/personapp/core/service/PersonaService.java
@@ -2,6 +2,7 @@ package co.edu.javeriana.as.personapp.core.service;
 
 import java.util.List;
 
+import co.edu.javeriana.as.personapp.core.domain.Profesion;
 import co.edu.javeriana.as.personapp.core.port.out.rest.PersonaRestPort;
 import co.edu.javeriana.as.personapp.core.usecase.PersonaUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,34 +26,78 @@ public class PersonaService implements PersonaUseCase {
 
 	@Override
 	public Persona buscarPorId(Integer cc) {
-		return personaMongoPort.findByCC(cc);
+		Persona mongo = personaMongoPort.findByCC(cc);
+		Persona mySQL = personaMySQLPort.findByCC(cc);
+		Persona rest = personaRestPort.findByCC(cc);
+
+		if(mongo.equals(rest) && mongo.equals(mySQL))
+			return mongo;
+
+		return null;
 	}
 
 	@Override
 	public List<Persona> buscarTodo() {
-		return personaMongoPort.findAll();
+		List<Persona> mongo = personaMongoPort.findAll();
+		List<Persona> mySQL = personaMySQLPort.findAll();
+		List<Persona> rest = personaRestPort.findAll();
+
+		if(
+				(mongo.size() == mySQL.size() && rest.size() == mySQL.size())
+						&& (mongo.equals(mySQL) && mongo.equals(rest))
+		) {
+			return mongo;
+		}
+
+		return null;
 	}
 
 	@Override
 	public Integer contar() {
-		return personaMongoPort.count();
+		Integer mongo = personaMongoPort.count();
+		Integer mySQL = personaMySQLPort.count();
+		Integer rest = personaRestPort.count();
+
+		if(mongo == mySQL && mongo == rest)
+			return rest;
+
+		return null;
 	}
 
 	@Override
 	public Persona crear(Persona persona) {
-		return personaMongoPort.update(persona);
+		Persona mongo = personaMongoPort.save(persona);
+		Persona mySQL = personaMySQLPort.save(persona);
+		Persona rest = personaRestPort.save(persona);
+
+		if(mongo != null && mySQL != null && rest != null)
+			return mongo;
+
+		return null;
 	}
 
 	@Override
 	public Persona editar(Integer cc, Persona persona) {
-		// TODO Auto-generated method stub
+		Persona mongo = personaMongoPort.save(persona);
+		Persona mySQL = personaMySQLPort.save(persona);
+		Persona rest = personaRestPort.save(persona);
+
+		if(mongo != null && mySQL != null && rest != null)
+			return mongo;
+
 		return null;
 	}
 
 	@Override
 	public Boolean eliminar(Integer cc) {
-		// TODO Auto-generated method stub
-		return null;
+		Boolean mongo = personaMongoPort.delete(cc);
+		Boolean mySQL = personaMySQLPort.delete(cc);
+		Boolean rest = personaRestPort.delete(cc);
+
+		if(mongo && mySQL && rest)
+			return mongo;
+
+		return false;
 	}
 
 }
